395fe0f38f46c8f5a01506fd83d214cf1106d458
Häkkänen Joni (FMI) <joni.hakkanen@fmi.fi>
2024-04-16 12:42:49 +0300

    small fixes

--- meteogram/config.js
+++ meteogram/config.js
@@ -1,12 +1,13 @@
 var graphconf = {
   serviceURL: 'http://smartmet.fmi.fi/timeseries?',
   producer: 'ecmwf_maailma_pinta',
-  timestep: '120',
-  timesteps: '30',
+  timestep: '120', // how much minutes between points
+  timesteps: '30', // how many points to draw on the graph
   lang: 'en', // en, fi, sv
   locations: {
     'Helsinki': '60,25',
     'Tampere': '61.5,23.8',
     'Oulu': '65.0,25.5'
+    // add more locations here
   }
 }
--- weathermap/map-config.js
+++ weathermap/map-config.js
@@ -6,15 +6,16 @@ var now = dd.toISOString()
 
 
 var mapconfigurations = {
-  debug: true,
-  wmsserver: 'http://smartmet.fmi.fi/wms',
+  debug: true, // debug mode for development, messages are printed to console
+  wmsserver: 'http://smartmet.fmi.fi/wms', 
   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Finnish Meteorological Institute',
   mapCenter: [60, 25], // lat, lon
   bounds: {north: 72.0, east: 35.0, south: 55.0, west: 20.0},
   // wms layers
   layers: {
-    'ECMWF 2m Temperature': 'fmi:ecmwf:rawtemperature',
-    'ECMWF Total Cloud Cover': 'fmi:ecmwf:totalcloudcover'
+    'ECMWF Total Cloud Cover': 'fmi:ecmwf:totalcloudcover',
+    'ECMWF Precipitation': 'fmi:ecmwf:precipitation',
+    'ECMWF 2m Temperature': 'fmi:harmonie:temperature', // fmi:gfs:sfc:temperature || fmi:ecmwf:rawtemperature
   },
   wmsOpacity: '0.8',
   wmsTileSize: 2048,
@@ -41,5 +42,4 @@ var mapconfigurations = {
     timeInterval: now + "/PT72H",
     period: "PT3H",
   }
-
 };
